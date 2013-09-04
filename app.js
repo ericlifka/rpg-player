955c744ff885d3e029e093ee0d6850b0950a74c8
Eric Lifka <eric.lifka@orgspan.com>
2013-09-04 15:04:23 -0400

    adds middleware for authenticating users based on auth_token cookie

--- app.js
+++ app.js
@@ -8,10 +8,12 @@ var auth = require('./routes/auth');
 var app = express();
 
 app.set('port', process.env.PORT || 3000);
+
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(express.cookieParser());
 app.use(app.router);
 app.use(require('less-middleware')({ src: __dirname + '/public' }));
 app.use(express.static(path.join(__dirname, 'public')));
@@ -22,7 +24,13 @@ if ('development' == app.get('env')) {
 
 app.post('/auth', auth.authPOST);
 app.put('/auth', auth.authPUT);
-app.delete('/auth', auth.authDELTE);
+app.delete('/auth', auth.authDELETE);
+
+app.get('/something', auth.authMiddleware, function (req, res) {
+    console.log('/something');
+    console.log(req.user);
+    res.send(200);
+});
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express server listening on port ' + app.get('port'));
--- routes/auth.js
+++ routes/auth.js
@@ -6,38 +6,53 @@ var authPOST = function (req, res) {
         timeStamp = new Date(),
         token = username + ":" + timeStamp.getTime();
 
-    if (username && password) {
-        sessions[token] = username;
-        sendAuthResponse(username, token, res);
-    } else {
-        res.send(401);
+    if (!username || !password) {
+        return res.send(401);
     }
+
+    sessions[token] = username;
+    return sendAuthResponse(username, token, res);
 };
 
 var authPUT = function (req, res) {
     var token = req.body.token,
         username = sessions[token];
 
-    if (token && username) {
-        sendAuthResponse(username, token, res);
-    } else {
-        res.send(401);
+    if (!token || !username) {
+        return res.send(401);
     }
+
+    return sendAuthResponse(username, token, res);
 };
 
-var authDELTE = function (req, res) {
+var authDELETE = function (req, res) {
     var token = req.body.token;
     sessions[token] = undefined;
     res.send(200);
 };
 
 var sendAuthResponse = function (username, token, res) {
-    res.send({
+    return res.send({
         token: token,
         username: username
     });
 };
 
-exports.authPOST = authPOST;
-exports.authPUT = authPUT;
-exports.authDELTE = authDELTE;
+var authenticationMiddleware = function (req, res, next) {
+    var token = req.cookies.auth_token,
+        username = sessions[token];
+
+    if (!username) {
+        return res.send(401);
+    }
+
+    req.user = username;
+    return next();
+};
+
+module.exports = {
+    authPOST: authPOST,
+    authPUT: authPUT,
+    authDELETE: authDELETE,
+    authMiddleware: authenticationMiddleware
+};
